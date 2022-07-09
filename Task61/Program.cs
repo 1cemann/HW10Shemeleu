bdaf3fd03651a2a58bc557442c0793a8061d33ab
Mad <ned_91@mail.ru>
2022-07-09 22:53:10 +0300

    Task61

---
+++ Task61/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 61: Показать треугольник Паскаля. 
+//*Сделать вывод в виде равнобедренного треугольника.
+
+
+using System;
+using static System.Console;
+Clear();
+
+Console.Write("Введите количество строк : ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int c = 1;
+    for (int j = 0; j < n - i; j++)
+    {
+        Write("   ");
+    }
+
+    for (int k = 0; k <= i; k++)
+    {
+        Write("   {0:D} ", c);
+        c = c * (i - k) / (k + 1);
+    }
+    WriteLine();
+    
+}
+
+
+
